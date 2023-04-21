9b840d3beb999075c733c7ef720fa79a73df6696
ryujin-akm <109247925+ryujin-akm@users.noreply.github.com>
2023-04-21 18:14:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
+# sudo apt-get install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
+apt-get update
+apt-get install -y libsndfile1 libasound2-dev portaudio19-dev libportaudio2 libportaudiocpp0
+
 
 pip install pyaudio
 
