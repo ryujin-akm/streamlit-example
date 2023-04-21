db1c0265c06e2d4cee4860005cb9f3249aa42f41
ryujin-akm <109247925+ryujin-akm@users.noreply.github.com>
2023-04-21 20:29:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,6 @@
 import streamlit as st
-import sounddevice as sd
-import soundfile as sf
+from audio_recorder_streamlit import audio_recorder
 
-def main():
-    st.title("Audio Recorder and Transcriber")
-    
-    # Display instructions
-    st.write("Click the button to start recording.")
-    
-    # Create button to start recording
-    if st.button("Record"):
-        # Record audio
-        duration = 5  # seconds
-        sample_rate = 44100  # Hz
-        channels = 2  # stereo
-        audio_file = "recording.wav"
-        myrecording = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=channels)
-        sd.wait()
-        sf.write(audio_file, myrecording, sample_rate)
-        st.write("Recording finished.")
-        st.audio(audio_file, format='audio/wav')
-    
-if __name__ == "__main__":
-    main()
-
+audio_bytes = audio_recorder()
+if audio_bytes:
+    st.audio(audio_bytes, format="audio/wav")
