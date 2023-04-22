1aa9239397f68fb5f71c8277efd858bfb523df23
ryujin-akm <109247925+ryujin-akm@users.noreply.github.com>
2023-04-22 15:17:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,8 @@ if len(audio) > 0:
 #     "ffmpeg", "-i", input_file, "-acodec", "pcm_s16le", "-ar", "44100",
 #     output_file
 # ])
-
+res = subprocess.call('ls l', shell=True)
+st.write(res)
 # convert mp3 file to wav
 sound = AudioSegment.from_mp3("audio.mp3")
 sound.export("transcript.wav", format="wav")
