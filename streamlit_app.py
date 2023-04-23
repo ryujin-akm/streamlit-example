a6392e527e171607b77c593a53bb596897a8cbaa
ryujin-akm <109247925+ryujin-akm@users.noreply.github.com>
2023-04-23 13:28:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,9 +63,28 @@ AUDIO_FILE = "transcript.wav"
 r = sr.Recognizer()
 with sr.AudioFile(AUDIO_FILE) as source:
     print("I am Here")
+    r.adjust_for_ambient_noise(source,duration=2)
     audio = r.record(source)  # read the entire audio file
 #     text = recognize.recognize_google(audio, language='en-IN', show_all=True)
-    print("Transcription: " + str(r.recognize_google(audio, language = 'en-IN', show_all=True)))
+#     print("Transcription: " + str(r.recognize_google(audio, language = 'en-IN', show_all=True)))
+# recognize speech using Sphinx
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
+
+# recognize speech using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
 
