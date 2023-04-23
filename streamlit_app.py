18347949a73eba72553e02d33b10e8bd584bfd9d
ryujin-akm <109247925+ryujin-akm@users.noreply.github.com>
2023-04-23 16:51:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ with sr.AudioFile(AUDIO_FILE) as source:
 # recognize speech using Sphinx
 try:
     print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
+except:
     print("Sphinx could not understand audio")
 except sr.RequestError as e:
     print("Sphinx error; {0}".format(e))
